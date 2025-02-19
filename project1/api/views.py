5559a45e96af73b7853fd40eb8476c59ef27a70d
HemantEmpiric <hemant@empiricinfotech.com>
2025-02-19 18:18:31 +0530

    test commit

---
+++ project1/api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer 
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+
+def student_details(request):
+
+    #complex dta
+    stu=Student.objects.get(id=1)
+    print('stu: ', stu)
+
+    #python data
+    serializer=StudentSerializer(stu)
+    print('serializer: ', serializer)
+    print('serializer: ', serializer.data)
+
+    #json data
+    # json_data=JSONRenderer().render(serializer.data)
+    # print('json_data: ', json_data)
+    # return HttpResponse(json_data , content_type='application/json')
+    
+    #use jsonresponse
+    return JsonResponse(serializer.data)
