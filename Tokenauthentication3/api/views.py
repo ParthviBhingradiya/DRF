87831ff062be65e9e2ec02cf185e1584157c430b
HemantEmpiric <hemant@empiricinfotech.com>
2025-02-26 19:03:28 +0530

    test commit

---
+++ Tokenauthentication3/api/views.py
@@ -0,0 +1,16 @@
+from .models import Student
+from .serializer import Studentserializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=Studentserializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+
+
+
+
