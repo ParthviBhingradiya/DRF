9da9c0846b3312f86e8d1ebbeecf469a4e2e4301
HemantEmpiric <hemant@empiricinfotech.com>
2025-03-03 19:00:50 +0530

    test commit

---
+++ SerializerRelations/api/serializers.py
@@ -0,0 +1,16 @@
+from .models import Singer,Song
+from rest_framework import serializers
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Song
+        fields=['id','title','singer','duration']
+
+class SingerSerializer(serializers.ModelSerializer):
+    # song=serializers.StringRelatedField(many=True,read_only=True)
+    song=serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='song-deails')
+
+    class Meta:
+        model=Singer
+        fields=['id','name','gender','song']
+
